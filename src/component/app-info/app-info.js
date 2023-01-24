1bd6a1da811026d1664a467a3199fbed5f3fa49d
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-01-24 18:47:35 +0200

    Added componetn

---
+++ src/component/app-info/app-info.js
@@ -0,0 +1,13 @@
+import'./app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрудников компании</h1> 
+      <h2>Общее чесло сотрудников: </h2>
+      <h2>Премия:</h2>
+    </div>
+  )
+};
+
+export default AppInfo;
